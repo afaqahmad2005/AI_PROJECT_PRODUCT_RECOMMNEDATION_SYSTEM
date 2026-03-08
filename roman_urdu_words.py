1daba2bce13ccf50f40919a7f58d22f3a95aa460
afaqahmad2005 <afaqshewa1234@gmail.com>
2026-03-08 20:17:03 +0500

    Done

---
+++ roman_urdu_words.py
@@ -0,0 +1,29 @@
+# Roman Urdu Sentiment Word Lists
+# Use: from roman_urdu_words import ROMAN_URDU_POSITIVE, ROMAN_URDU_NEUTRAL, ROMAN_URDU_NEGATIVE
+
+ROMAN_URDU_POSITIVE = [
+    'acha', 'achha', 'accha', 'zabardast', 'lajawab', 'kamaal', 'perfect',
+    'shandar', 'behtreen', 'behtar', 'khoob', 'khubsurat',
+    'mast', 'maja', 'sahi', 'badiya', 'badhiya',
+    'excellent', 'khushi', 'pasand', 'pyara', 'dilchasp', 'mazedaar',
+    'laziz', 'lajawaab', 'Zabardast', 'dhamaal', 'maza', 'achi', 'best',
+    'outstanding', 'wonderful', 'fantastic', 'superb', 'amazing', 'great',
+    'bohat acha', 'bohat achha', 'highly', 'recommend',
+    'good', 'awesome', 'nice', 'love', 'loved', 'like', 'liked', 'satisfied', 'happy', 'enjoy', 'enjoyed', 'positive', 'super', 'top', 'top class', 'top-class', 'value for money', 'worth it', 'worth buying'
+]
+
+ROMAN_URDU_NEUTRAL = [
+    'theek', 'thik', 'ok', 'okay', 'average', 'normal', 'chalega',
+    'chal jayega', 'decent', 'moderate', 'so so', 'so-so', 'manageable',
+    'acceptable', 'aam', 'theek thaak', 'theek thak', 'medium', 'ordinary'
+]
+
+ROMAN_URDU_NEGATIVE = [
+    'ghatiya', 'kharab', 'bekaar', 'bekar', 'fazool', 'fuzool', 'bakwas',
+    'wahiyat', 'faltu', 'ganda', 'bura', 'kharaab', 'nautanki', 'dhoka',
+    'dhokha', 'badtamiz', 'kamzor', 'kamzoor', 'masla', 'maslay', 'problem',
+    'worst', 'bad', 'terrible', 'poor', 'useless', 'pathetic', 'disappointing',
+    'disappointing', 'falana', 'bekar', 'kachra', 'third class', 'third-class',
+    'fikar', 'pareshan', 'gussa', 'nafrat', 'mushkil', 'takleef', 'dard',
+    'nahi pasand', 'na pasand', 'not good', 'waste', 'bakwaas', 'khatam','nahi acha','not good', 
+]
